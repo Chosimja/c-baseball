ff5e92482fe3afaeca960676edc786109853278a
Chosimja <chosimja@proton.me>
2022-08-06 12:21:55 +0900

    baseball

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int *p;
+    int i, strike;
+    int one, two, three;
+    printf("0~9까지 3자리 숫자를 입력하시오 : ");
+    scanf("%d %d %d", &one, &two, &three);
+    printf("%d %d %d\n", one, two, three);
+}
