93cf005038cc03131f05ece37648875d129fb084
Chosimja <chosimja@proton.me>
2022-08-08 09:12:18 +0900

    숫자야구

--- main.c
+++ main.c
@@ -3,10 +3,20 @@
 #include <time.h>
 
 int main(){
+    // 변수 선언
     int *p;
-    int i, strike;
+    int i, strike, random[3];
     int one, two, three;
+    // 숫자 입력 받는 곳
     printf("0~9까지 3자리 숫자를 입력하시오 : ");
     scanf("%d %d %d", &one, &two, &three);
-    printf("%d %d %d\n", one, two, three);
+    // 난수 발생
+    srand(time(NULL));
+    for (i = 0; i<=3; i++){
+        random[i] = rand()%9;
+        printf("%d", random[1]);
+    }
+    
+
+
 }
